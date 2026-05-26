e572d8714a1edc67803c5216c6b83df1d4579c62
tasnimah-lgtm <tasnimah@umich.edu>
2026-05-26 08:31:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+ const button = document.getElementById("topBtn");
+
+button.addEventListener("click", function () {
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
+
